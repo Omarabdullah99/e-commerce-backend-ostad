a147a9458e111a2b58e80673788f865879611017
Omarabdullah99 <omarabdullah917303@gmail.com>
2025-02-14 17:15:00 +0600

    Creat and get AllProfile

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,3 +1,4 @@
+const ProfileModel = require("../models/userRelatedModel/ProfileModel")
 const UserModel = require("../models/userRelatedModel/UsersModel")
 
 
@@ -22,8 +23,29 @@ const GetAllUsers= async(req,res)=>{
     }
 }
 
+const CreateProfileUser= async(req,res)=>{
+    try {
+        let data= req.body;
+        const result= await ProfileModel.create(data)
+        res.status(201).json(result)
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+const GetAllProfileUser= async(req,res)=>{
+    try {
+        const result= await ProfileModel.find()
+        res.status(200).json(result)
+        
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
 
 //------------------users related function end----------------
 
 
-module.exports={CreateUser,GetAllUsers}
+module.exports={CreateUser,GetAllUsers,CreateProfileUser,GetAllProfileUser}
--- routers/UserRouter.js
+++ routers/UserRouter.js
@@ -1,9 +1,12 @@
 const express= require('express')
-const {  CreateUser, GetAllUsers } = require('../controllers/UsersController')
+const {  CreateUser, GetAllUsers, CreateProfileUser, GetAllProfileUser } = require('../controllers/UsersController')
 
 const UserRouter= express.Router()
 
 
 UserRouter.post('/createUser',CreateUser)
 UserRouter.get("/getAllUsers",GetAllUsers)
+
+UserRouter.post('/createProfileUser',CreateProfileUser)
+UserRouter.get('/getAllProfileUser',GetAllProfileUser)
 module.exports= UserRouter
